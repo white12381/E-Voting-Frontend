e802dc100ba4350085be359d36cada899b122892
Olasunkanmi Usman <Olasunkanmiusman1111@gmail.com>
2025-06-20 22:34:16 +0100

    `Refactor LineLoader component to use a different loading animation`

--- src/component/lineLoader.jsx
+++ src/component/lineLoader.jsx
@@ -1,6 +1,15 @@
 const LineLoader = () => {
-    return <div className="text-center text-primary font-bold text-sm md:text-xl "> <div class="loaders mx-auto my-8"></div> 
-    <p>Loading</p>
+    return     <div className="p-4 border rounded-[30px]">
+      <div className="border-b border-gray-200 mb-4">
+        <div className="w-[100%] h-12 bg-gradient-loading mb-6"></div>
+        <div className="w-[100%] h-12 bg-gradient-loading mb-6"></div>
+        <div className="w-[100%] h-12 bg-gradient-loading mb-6"></div>
+        <div className="w-[100%] h-12 bg-gradient-loading mb-6"></div>
+        <div className="w-[100%] h-12 bg-gradient-loading mb-6"></div>
+        <div className="w-[100%] h-12 bg-gradient-loading mb-6"></div>
+        <div className="w-[100%] h-12 bg-gradient-loading mb-6"></div>
+      </div>
     </div>
+    
 }
 export default LineLoader
